03bf6c3582034baff69c1df7e0358d810747eb52
wanglf1207 <wanglf1207@163.com>
2020-10-20 17:42:03 +0800

    initcommit

---
+++ src/main/java/com/cn/selfroom/she/server/Vue2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.cn.selfroom.she.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vue2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vue2ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cn/selfroom/she/server/Vue2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.selfroom.she.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vue2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
